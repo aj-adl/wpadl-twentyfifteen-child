3a63a5ccc6620576547e80c19669d30a4a32bcb9
Alex James Bishop <alex@bshpdigital.com>
2015-01-14 00:20:24 +1030

    Add Modernizr to header function
    Includes a custom modernizr build inline in the <head>

--- functions.php
+++ functions.php
@@ -8,3 +8,18 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-theme-stylesheet', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-theme-stylesheet', get_stylesheet_uri(), array( 'parent-theme-stylesheet' ) );
 }
+
+// I choose to inline modernizr in the <head>
+// - Best practice according to them
+// - Be sure to use a custom build
+
+// An example of outputting inline javascript in the <head>
+// Changing the action from 'wp_head' to 'wp_footer' would place it before </body> tag
+
+add_action( 'wp_head', 'BSHP_inline_modernizr', 9 );
+function BSHP_inline_modernizr() {
+  
+  include_once('inc/inline_modernizr.php');
+
+  echo $modernizr;
+}
