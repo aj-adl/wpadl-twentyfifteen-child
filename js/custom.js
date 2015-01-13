a65e0b354e64360a5d9ee9cfe8a2eefb96903744
Alex James Bishop <alex@bshpdigital.com>
2015-01-14 00:56:27 +1030

    Tweak to scrollReveal.js
    It now shows when 15% of object is in viewport
    
    Prev setting was too high so long articles suffered from lots of delay

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,13 @@
 // Standard No Conflict wrappers. 
 jQuery(document).ready(function($) {
 
+	// Scroll Reveal Configuration 
+
+	var srConfig = {
+		vFactor: 0.15,
+	};
+
 	// Initialize Scroll Reveal
-	window.sr = new scrollReveal();
+	window.sr = new scrollReveal( srConfig );
 
 });
